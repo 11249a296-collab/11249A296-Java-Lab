adde0b6adc92c952f0eb0ad8ba8d946d2bc4dcee
Pradhamesh M <11249a296@kanchiuniv.ac.in>
2026-08-30 12:33:23 +0530

    Packages

---
+++ expt 9/Main.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.Scanner;
+import studentInfo.Student;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Student s = new Student();
+
+        s.displayInfo();
+    }
+}
---
+++ expt 9/studentInfo/Student.java
@@ -0,0 +1,11 @@
+package studentInfo;
+import java.io.*;
+import java.util.Scanner;
+
+public class Student {
+    public void displayInfo() {
+        System.out.println("Student Name: Ramesh");
+        System.out.println("Register No: 101");
+        System.out.println("Department: Computer Science");
+    }
+}
